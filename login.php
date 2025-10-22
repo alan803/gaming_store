0f845de2009a6b3adfefae9e6ece5cb574ebb8fe
alan803 <alanvarghese2027@mca.ajce.in>
2025-10-22 11:58:06 +0530

    Update navigation links in login.php

--- login.php
+++ login.php
@@ -98,10 +98,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 </div>
                 <div class="hidden md:flex space-x-8">
                     <a href="homepage.php" class="nav-link text-gray-600 hover:text-indigo-600">Home</a>
-                    <a href="#" class="nav-link text-gray-600 hover:text-indigo-600">Products</a>
-                    <a href="#" class="nav-link text-gray-600 hover:text-indigo-600">Categories</a>
-                    <a href="#" class="nav-link text-gray-600 hover:text-indigo-600">Deals</a>
-                    <a href="#" class="nav-link text-gray-600 hover:text-indigo-600">About</a>
+                    <a href="homepage.php" class="nav-link text-gray-600 hover:text-indigo-600">About</a>
+                    <a href="products.php" class="nav-link text-gray-600 hover:text-indigo-600">Shop</a>
                 </div>
                 <div class="flex items-center space-x-6">
                     <button class="text-gray-600 hover:text-indigo-600 transition-colors">
